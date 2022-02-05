0255416a3b4fa1e5e555435768926deae75b29a3
John Santiago <johnsantiago@Johns-Mac-mini.COMFAST>
2022-02-04 18:23:00 -0700

    Struct story made with viewDidLoad using array and struct

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,8 +14,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyArray : [Story] = []
+    
+    let story0 = Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right")
+    
+    let story1 = Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead.")
+    
+    let story2 = Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps.")
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
+        
+        storyArray.append(story0)
+        storyArray.append(story1)
+        storyArray.append(story2)
+        
+        storyLabel.text = storyArray[0].title
+        choice1Button.setTitle(storyArray[0].choice1, for: .normal)
+        choice2Button.setTitle(storyArray[0].choice2, for: .normal)
+        	
+        
 
     }
 
